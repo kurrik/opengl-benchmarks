b7b3bdee0f321c9ec889af1a0ba2f53f8866ac8a
Arne Roomann-Kurrik <kurrik@gmail.com>
2015-08-24 23:28:59 -0700

    Boilerplate code for window creation

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+GITROOT=`git rev-parse --show-toplevel`
+
+export GOPATH="$GITROOT/gocode"
+mkdir -p $GOPATH
+
+go get github.com/go-gl/gl/v3.3-core/gl
+go get github.com/go-gl/glfw/v3.1/glfw
+
+go run $GITROOT/src/uniform-texture-coords/*.go
---
+++ src/uniform-texture-coords/main.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Arne Roomann-Kurrik
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"../common"
+	"runtime"
+)
+
+func init() {
+	// See https://code.google.com/p/go/issues/detail?id=3527
+	runtime.LockOSThread()
+}
+
+func main() {
+	const (
+		WinTitle  = "uniform-texture-coords"
+		WinWidth  = 640
+		WinHeight = 480
+	)
+
+	var (
+		context *common.Context
+		err     error
+	)
+	if context, err = common.NewContext(); err != nil {
+		panic(err)
+	}
+	if err = context.CreateWindow(WinWidth, WinHeight, WinTitle); err != nil {
+		panic(err)
+	}
+	for !context.ShouldClose() {
+		context.Events.Poll()
+		context.SwapBuffers()
+	}
+}
